642b3e3fc38b722d5fb522373fc4f5e5a64a957d
Konstantin Starodubenko <KonstantinS@yandex.ru>
2018-04-27 18:04:57 +0600

    changed style

---
+++ project_1/js/myJS/logic.js
@@ -0,0 +1,10 @@
+// $( document ).ready(function() {
+//
+//     $( ".portfolio-item " ).hover(
+//         function() {
+//             $(".effect").css("filter","grayscale(100%)");
+//         }, function() {
+//             $(".effect").css("filter","");
+//         }
+//     );
+// });
